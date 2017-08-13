e3905f4f75b4cd5bbfb35ae366a80dcc0855f6fb
YiXiang <s9801077@gmail.com>
2017-08-13 14:48:20 +0800

    2. 新增商品

--- src/containers/Home/components/Content.js
+++ src/containers/Home/components/Content.js
@@ -1,5 +1,9 @@
-import React, { Component } from 'react';
-import { Container, Row, Col, Button, Jumbotron } from 'reactstrap';
+import React, {Component} from 'react';
+import {
+  Container, Row, Col, Button, Jumbotron, Card, CardImg, CardText, CardBlock,
+  CardTitle, CardSubtitle
+} from 'reactstrap';
+import AlbumJSON from './Album.json';
 
 export default class Content extends Component {
   render() {
@@ -8,17 +12,35 @@ export default class Content extends Component {
         <Row>
           <Col md="12">
             <Jumbotron>
-              <h1 className="display-3">Hello, world!</h1>
-              <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
-              <hr className="my-2" />
-              <p>It uses utility classes for typgraphy and spacing to space content out within the larger container.</p>
+              <h1 className="display-3">你好，世界！</h1>
+              <p className="lead">This is a simple shop.</p>
+              <hr className="my-2"/>
+              <p>It looks good!</p>
               <p className="lead">
                 <Button color="primary">Learn More</Button>
               </p>
             </Jumbotron>
           </Col>
         </Row>
-
+        <Row>
+          {
+            AlbumJSON.map(product => (
+              <Col xs="12" sm="6" md="4">
+                <Card>
+                  <CardImg top width="100%"
+                           src={product.img}
+                           alt="Card image cap"/>
+                  <CardBlock>
+                    <CardTitle>{product.title}</CardTitle>
+                    <CardSubtitle>${product.price}</CardSubtitle>
+                    <CardText>{product.desc}</CardText>
+                    <Button color="success">Buy</Button>
+                  </CardBlock>
+                </Card>
+              </Col>
+            ))
+          }
+        </Row>
       </Container>
     );
   }
