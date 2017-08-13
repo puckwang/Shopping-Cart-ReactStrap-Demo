0327b7279b93a67f880434bd2b31b43a2cd6126b
YiXiang <s9801077@gmail.com>
2017-08-13 16:03:17 +0800

    3. 新增購物車

--- src/containers/Home/components/Content.js
+++ src/containers/Home/components/Content.js
@@ -1,12 +1,44 @@
 import React, {Component} from 'react';
 import {
   Container, Row, Col, Button, Jumbotron, Card, CardImg, CardText, CardBlock,
-  CardTitle, CardSubtitle
+  CardTitle, CardSubtitle, Modal, ModalHeader, ModalBody, ModalFooter, Badge, CardColumns, Table
 } from 'reactstrap';
 import AlbumJSON from './Album.json';
 
 export default class Content extends Component {
+  state = {
+    modal: false,
+    cart: [],
+  }
+
+  toggle = () => {
+    this.setState({
+      modal: !this.state.modal
+    });
+  }
+
+  addToCart = (prod) => {
+    const newCart = this.state.cart;
+    newCart.push(prod);
+    this.setState({
+      cart: newCart
+    })
+  };
+
+  delToCart = (prod) => {
+    const newCart = this.state.cart.filter(item => item.id !== prod.id);
+    this.setState({
+      cart: newCart
+    })
+  };
+
+  checkout = (totalPrice) => {
+    alert(`已結帳 ${totalPrice} 元`);
+  }
+
   render() {
+    const TotalPrice = this.state.cart.reduce((acc, item) => acc + item.price, 0);
+
     return (
       <Container>
         <Row>
@@ -17,30 +49,69 @@ export default class Content extends Component {
               <hr className="my-2"/>
               <p>It looks good!</p>
               <p className="lead">
-                <Button color="primary">Learn More</Button>
+                <Button color="primary" onClick={this.toggle}>購物車({this.state.cart.length})</Button>
               </p>
             </Jumbotron>
           </Col>
         </Row>
-        <Row>
+        <CardColumns>
           {
             AlbumJSON.map(product => (
-              <Col xs="12" sm="6" md="4">
-                <Card>
-                  <CardImg top width="100%"
-                           src={product.img}
-                           alt="Card image cap"/>
-                  <CardBlock>
-                    <CardTitle>{product.title}</CardTitle>
-                    <CardSubtitle>${product.price}</CardSubtitle>
-                    <CardText>{product.desc}</CardText>
-                    <Button color="success">Buy</Button>
-                  </CardBlock>
-                </Card>
-              </Col>
+              <Card>
+                <CardImg top width="100%"
+                         src={product.img}
+                         alt="Card image cap"/>
+                <CardBlock>
+                  <CardTitle>{product.title}</CardTitle>
+                  <CardSubtitle>
+                    <h5>
+                      {
+                        product.discount ?
+                          <Badge color="primary" pill>售價: ${product.price}</Badge> :
+                          <Badge color="danger" pill>特價: ${product.price}</Badge>
+                      }
+                    </h5>
+                  </CardSubtitle>
+                  <CardText>{product.desc}</CardText>
+                  <Button disabled={this.state.cart.find(item => item.id === product.id)} color="success"
+                          onClick={() => this.addToCart(product)}>Buy</Button>
+                </CardBlock>
+              </Card>
             ))
           }
-        </Row>
+        </CardColumns>
+        <Modal isOpen={this.state.modal} toggle={this.toggle}>
+          <ModalHeader toggle={this.toggle}>購物車</ModalHeader>
+          <ModalBody>
+            <Table>
+              <thead>
+              <tr>
+                <th>#</th>
+                <th>品項</th>
+                <th>價格</th>
+                <th>{' '}</th>
+              </tr>
+              </thead>
+              <tbody>
+              {
+                this.state.cart.map((prod, index) => (
+                  <tr>
+                    <th scope="row">{index + 1}</th>
+                    <td>{prod.title}</td>
+                    <td>{prod.price}</td>
+                    <td><Button color="danger" size="sm" onClick={() => this.delToCart(prod)}>X</Button></td>
+                  </tr>
+                ))
+              }
+              </tbody>
+            </Table>
+            <p>總價: ${TotalPrice}</p>
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={() => this.checkout(TotalPrice)}>結帳</Button>{' '}
+            <Button color="secondary" onClick={this.toggle}>取消</Button>
+          </ModalFooter>
+        </Modal>
       </Container>
     );
   }
